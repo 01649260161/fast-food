0014c36f9f4c20bb911011517da40735402cc0f3
Tran Minh Hien <tranminhhien130398@gmail.com>
2020-06-30 16:17:49 +0700

    abc

--- project/app/Providers/AppServiceProvider.php
+++ project/app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 use App\Model\Admin\ConfigModel;
+use App\Model\Admin\ContactModel;
 use App\Model\Admin\MenuItemModel;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
@@ -26,6 +27,30 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $items1 = ContactModel::all();
+        $contact =array();
+        $contact[] ='email';
+        $contact[] ='phone';
+        $contact[] ='address';
+        $contact[] ='web_intro';
+
+        $default1 =array();
+
+        //Tạo Mặc định cho mảng contact
+        foreach ($contact as $item_contact){
+
+            if (!isset($items1[$item_contact])){
+                $default1[$item_contact] = '';
+            }
+        }
+        
+        //Lấy từ CSDL ra đè lại mảng $default1
+
+        foreach ($items1 as $item){
+            $key = $item->name;
+            $default1[$key]=$item->value;
+        }
+
 
         $items = ConfigModel::all();
 
@@ -59,6 +84,7 @@ class AppServiceProvider extends ServiceProvider
         }
 
         $global_setting=$default;
+        $global_contact=$default1;
         //
         Schema::defaultStringLength(191);
         $menus_items_header =MenuItemModel::getMenuItemsByHeader();
@@ -70,6 +96,7 @@ class AppServiceProvider extends ServiceProvider
 
 
         View::share('fe_global_setting',$global_setting);
+        View::share('fe_global_contact',$global_contact);
         //View::share('fe_total_qtt_cart',$total_qtt_cart);
         View::share('fe_menus_items_header',$menus_items_header);
         View::share('fe_menus_items_header_html',$menus_items_header_html);
